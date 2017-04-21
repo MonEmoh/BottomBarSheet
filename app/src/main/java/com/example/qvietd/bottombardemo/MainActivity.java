cb1cf88251e1e8f2104d9121725b9a904d2b962b
MonEmoh <dimuakeo123@outlook.com.vn>
2017-04-21 11:03:05 +0700

    BarSheet Example

---
+++ app/src/main/java/com/example/qvietd/bottombardemo/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.qvietd.bottombardemo;
+
+import android.os.Bundle;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+        disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Utils.startFragment(getSupportFragmentManager(), ContentFragment.newInstance("ONE"));
+
+    }
+
+    private boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorites:
+                Utils.startFragment(getSupportFragmentManager(), ContentFragment.newInstance("ONE"));
+                break;
+            case R.id.action_schedules:
+                Utils.startFragment(getSupportFragmentManager(), ContentFragment.newInstance("TWO"));
+                break;
+            case R.id.action_music:
+                Utils.startFragment(getSupportFragmentManager(), ContentFragment.newInstance("THREE"));
+                break;
+        }
+        return true;
+    }
+
+    public void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e(TAG, "Unable to get shift mode field");
+        } catch (IllegalAccessException e) {
+            Log.e(TAG, "Unable to change value of shift mode");
+        }
+    }
+}
